bfddd523557ca7fc1b24c7b0d01056e6cd981800
xin <1844985716@qq.com>
2022-09-14 22:31:53 +0800

    feat: add post methods

--- hooks.js
+++ hooks.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 
 app.get('/front', (req, res) => {
-  console.log(req.headers);
-  res.send({success: true});
+    console.log('get front');
+    res.send({success: true});
 })
 
 app.get('/backed', (req, res) => {
-  console.log(req.headers);
-  res.send({success: true});
+    console.log('get backed');
+    res.send({success: true});
 })
 
+app.post('/backed', (req, res) => {
+    console.log(req.headers);
+    res.send({success: true});
+})
+
+app.post('/front', (req, res) => {
+    console.log(req.headers);
+    res.send({success: true});
+})
 
 app.listen(3000, () => {
   console.log('server running at localhost:3000');
