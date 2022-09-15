93b3330d50a061fe801405b0794083ffd8f7bc74
xin <1844985716@qq.com>
2022-09-15 23:46:29 +0800

    feat: update front.sh

--- front.sh
+++ front.sh
@@ -2,7 +2,8 @@
 cd /usr/code/web-app/
 git clean -f
 git reset --hard
+git pull origin master
 rm -rf /usr/share/nginx/html/dist
 rm -rf dist
 npm run build
-cp dist /usr/share/nginx/html
+cp dist /usr/share/nginx/html -a
